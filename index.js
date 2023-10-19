3ebef2ff20f6b1646e4577efc37ed3692ee5658b
ruslanrzayev1 <sd7ga5a2n@code.edu.az>
2023-10-19 23:44:13 +0400

    First Java

---
+++ index.js
@@ -0,0 +1,71 @@
+// let a = 5;
+// let b = 4;
+// if (a>b) {
+// console.log("a b-den boyukdur");
+// }
+// else if(a===b){
+//     console.log("beraberdir");
+// }
+// else{
+//     console.log("a b-den kicikdir");
+// }
+
+let students = [
+  { id: 1, name: "Ali", surname: "Alisoy", age: 20, grade: 95 },
+
+  { id: 2, name: "Vusala", surname: "Nabiyeva", age: 34, grade: 77 },
+
+  { id: 3, name: "Aliya", surname: "Gurbanzade", age: 14, grade: 98 },
+
+  { id: 4, name: "Sabir", surname: "Nuruyev", age: 15, grade: 54 },
+
+  { id: 5, name: "Mehman", surname: "Heydarov", age: 22, grade: 100 },
+
+  { id: 6, name: "Sevana", surname: "Mammadli", age: 41, grade: 35 },
+
+  { id: 7, name: "Ilham", surname: "Babayev", age: 24, grade: 95 },
+
+  { id: 8, name: "Namiq", surname: "Gulahmedov", age: 20, grade: 95 },
+
+  { id: 9, name: "Aygun", surname: "Kazimova", age: 24, grade: 100 },
+];
+
+//     let students2=[];
+
+//    for (const element of students) {
+//     if (element.id %2 == 0) {
+//         students2.push(element)
+//     }
+//    }
+//    console.log(students2);
+// for (let i = 0; i < students.length; i++) {
+//   if (students[i].id % 2 == 1) {
+//     console.log(students[i].name);
+//   }
+// }
+
+// let students3 = [];
+// for (const element of students) {
+//   if (element.age >= 20) {
+//     students3.push(element)
+//   }
+// }
+// console.log(students3);
+
+// let students4 = [];
+
+// for (const element of students) {
+//   if (element.id % 2 == 0 && element.grade >= 90) {
+//     students4.push(element)
+//   }
+// }
+// console.log(students4);
+
+// let students5 = [];
+
+// for (const element of students) {
+//   if (element.grade === 100 || element.grade === 95) {
+//     students5.push(element.name)
+//   }
+// }
+// console.log(students5);
